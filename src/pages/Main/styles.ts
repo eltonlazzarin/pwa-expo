8eceac2d48d7f37c148968948dfd85aaf55a7d5d
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-06-27 16:53:29 -0300

    style main page

---
+++ src/pages/Main/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  repo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+
+  avatar: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    marginRight: 16,
+  },
+});
+
+export default styles;
